27079d764b49007eb3e39e1015b8c5f5bd1f79f4
Binh Nguyen <ntbinh91@gmail.com>
2024-06-04 13:46:45 +0700

    Config tsconfig baseURL and babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src/'],
+        alias: {
+          src: './src',
+        },
+      },
+    ],
+  ],
 };
