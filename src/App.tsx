4649e36023d2157138e8e7b9f1d3f559b1b85ca9
Binh Nguyen <ntbinh91@gmail.com>
2024-06-04 15:36:38 +0700

    Add some neccessary libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {StarIcon, StopCircleIcon} from 'react-native-heroicons/outline';
+import Toast from 'react-native-simple-toast';
+import {
+  GluestackUIProvider,
+  Text,
+  SafeAreaView,
+  View,
+  Button,
+  ButtonText,
+} from '@gluestack-ui/themed';
+import {config} from '@gluestack-ui/config';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaView>
-        <Text>Hello App</Text>
-      </SafeAreaView>
-    </NavigationContainer>
+    <GluestackUIProvider config={config}>
+      <NavigationContainer>
+        <SafeAreaView>
+          <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
+            <Text>Hello App</Text>
+            <StarIcon color="red" />
+            <StopCircleIcon color="red" />
+          </View>
+
+          <Button
+            onPress={() => {
+              Toast.show('This is a toast message.', Toast.SHORT);
+            }}>
+            <ButtonText>Hello World</ButtonText>
+          </Button>
+        </SafeAreaView>
+      </NavigationContainer>
+    </GluestackUIProvider>
   );
 };
 
